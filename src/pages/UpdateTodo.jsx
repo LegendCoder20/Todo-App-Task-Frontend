b6aa6cd1d2e75b179736f399772ce99aeb05c54f
LegendCoder20 <20sit074.manjarekar.a20@gmail.com>
2025-08-11 13:15:57 +0530

    Fixed Update Todo Bug

--- src/pages/UpdateTodo.jsx
+++ src/pages/UpdateTodo.jsx
@@ -28,7 +28,7 @@ function UpdateTodo() {
     };
 
     getTodos();
-  }, []);
+  }, [id]);
 
   const updateTodo = async (e) => {
     e.preventDefault();
@@ -56,20 +56,20 @@ function UpdateTodo() {
 
   return (
     <>
-      <section class="bg-amber-100 ">
-        <div class="alertBox bg-green-300">
+      <section className="bg-amber-100 ">
+        <div className="alertBox bg-green-300">
           {err && <AlertBox message={err}></AlertBox>}
         </div>
-        <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
           <a
             href="#"
-            class="flex items-center mb-6 text-2xl font-semibold text-gray-900 "
+            className="flex items-center mb-6 text-2xl font-semibold text-gray-900 "
           >
-            <img class="w-8 h-8 mr-2" src={logo} alt="logo" />
+            <img className="w-8 h-8 mr-2" src={logo} alt="logo" />
             Todo App
           </a>
-          <div class="w-full bg-gray-100 rounded-lg shadow  md:mt-0 sm:max-w-md xl:p-0  ">
-            <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+          <div className="w-full bg-gray-100 rounded-lg shadow  md:mt-0 sm:max-w-md xl:p-0  ">
+            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <TodoForm
                 mode="updateTodo"
                 formData={formData}
