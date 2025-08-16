8cc1269a3024ac8a17179a9baee904ab44685100
LegendCoder20 <20sit074.manjarekar.a20@gmail.com>
2025-08-16 19:02:52 +0530

    Added Pagination

--- src/Main Pages/Dashboard.jsx
+++ src/Main Pages/Dashboard.jsx
@@ -10,23 +10,34 @@ function Dashboard() {
   const nav = useNavigate();
   const [todos, setTodos] = useState([]);
   const [message, setMessage] = useState("");
+  const [nextCursor, setNextCursor] = useState(null);
+  const [hasMore, setHasMore] = useState(true);
   const [loading, setLoading] = useState(false);
 
+  const limit = 10;
+
+  // Get Todos Function Made but Called inside UseEffect
+  const getTodos = async () => {
+    try {
+      const allTodos = await axios.get(
+        `${import.meta.env.VITE_API_URL}/todos`,
+        {params: {limit, cursor: nextCursor}, withCredentials: true}
+      );
+      setTodos((prev) => [...prev, ...allTodos.data.todos]);
+      setNextCursor(allTodos.data.nextCursor);
+      setHasMore(allTodos.data.hasMore);
+    } catch (err) {
+      nav("/");
+    } finally {
+      setLoading(false);
+    }
+  };
+
   useEffect(() => {
+    if (!hasMore) return;
+
     setLoading(true);
-    const getTodos = async () => {
-      try {
-        const allTodos = await axios.get(
-          `${import.meta.env.VITE_API_URL}/todos`,
-          {withCredentials: true}
-        );
-        setTodos(allTodos.data.todos);
-      } catch (err) {
-        nav("/");
-      } finally {
-        setLoading(false);
-      }
-    };
+
     getTodos();
   }, []);
 
@@ -69,13 +80,13 @@ function Dashboard() {
         <div className="text-center min-h-screen bg-blue-100">
           {message && <AlertBox message={message}></AlertBox>}
 
-          <div class="text-end relative top-2  m-auto w-auto  ">
+          <div className="text-end relative top-2  m-auto w-auto  ">
             <Button type="submit" color="red" onClick={logOut}>
               Logout
             </Button>
           </div>
 
-          <div class=" m-auto mt-4">
+          <div className=" m-auto mt-4">
             <Button
               type="submit"
               color="green"
@@ -86,37 +97,56 @@ function Dashboard() {
           </div>
 
           <div className="grid lg:grid-cols-3 md:grid-cols-2 w-auto  mt-10 mb-10 place-items-center  lg:gap-7 md:gap-12 gap-5  m-8 ">
-            {todos.map((todo) => (
-              <>
-                <div className="max-w-sm p-6 bg-gray-50  border border-gray-200 rounded-lg shadow-2xl  lg:w-80 md:w-80 sm:w-80 w-64  hover:scale-105 transition-transform duration-300">
-                  <p>
-                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 overflow-x-auto h-12">
-                      {todo.title}
-                    </h5>
-                  </p>
-                  <p className="mb-3 font-normal text-gray-700 h-14 overflow-y-auto ">
-                    {todo.description}
-                  </p>
-                  <Button
-                    type="submit"
-                    color="yellow"
-                    onClick={() => {
-                      nav(`/updateTodo/${todo._id}`);
-                    }}
+            {todos.map((todo) => {
+              const [year, month, day] = todo.createdAt
+                .split("T")[0]
+                .split("-");
+              const date = `${day}-${month}-${year}`;
+              return (
+                <>
+                  <div
+                    className="max-w-sm p-6 bg-gray-50  border border-gray-200 rounded-lg shadow-2xl  lg:w-80 md:w-80 sm:w-80 w-64  hover:scale-105 transition-transform duration-300"
+                    key={todo._id}
                   >
-                    Update Todo
-                  </Button>
-                  <Button
-                    type="submit"
-                    color="red"
-                    onClick={() => deleteTodoData(todo._id)}
-                  >
-                    Delete Todo
-                  </Button>
-                </div>
-              </>
-            ))}
+                    <p>
+                      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 overflow-x-auto h-12">
+                        {todo.title}
+                      </h5>
+                    </p>
+                    <p className="mb-3 font-normal text-gray-700 h-14 overflow-y-auto ">
+                      {todo.description}
+                    </p>
+                    <Button
+                      type="submit"
+                      color="yellow"
+                      onClick={() => {
+                        nav(`/updateTodo/${todo._id}`);
+                      }}
+                    >
+                      Update Todo
+                    </Button>
+                    <Button
+                      type="submit"
+                      color="red"
+                      onClick={() => deleteTodoData(todo._id)}
+                    >
+                      Delete Todo
+                    </Button>
+                    <br />
+                    <div className="text-right text-base font-semibold">
+                      {date}
+                    </div>
+                  </div>
+                </>
+              );
+            })}
           </div>
+          {hasMore && (
+            <button onClick={getTodos} disabled={loading}>
+              {loading ? <Loader /> : "Load More"}
+            </button>
+          )}
+          {!hasMore && "All Todos Loaded."}
         </div>
       ) : (
         <>
